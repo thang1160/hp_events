38c47560aa87fd27914d926593e0b773220953da
duynguyenfpt <39914280+duynguyenfpt@users.noreply.github.com>
2021-01-12 01:07:31 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json())
 
