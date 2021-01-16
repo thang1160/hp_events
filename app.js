16039a88a9117d012feb3521577effa25264cb29
duynguyenfpt <39914280+duynguyenfpt@users.noreply.github.com>
2021-01-16 15:09:10 +0700

    add images

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json())
 
+app.use(express.static(path.join(__dirname,'public')))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-app.get('/', (req, res) => {
-    res.send(`Listening on ${ PORT }`);
-})
+// app.get('/', (req, res) => {
+//     // res.send(`<h1>Listening on ${ PORT }</h1>`);
+//     res.sendFile(path.join((__dirname,'public','index.html')))
+// })
 
 module.exports = app;
