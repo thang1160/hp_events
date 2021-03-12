680b3b26590377f6690173b9710d844216649412
thang1160 <35557579+thang1160@users.noreply.github.com>
2021-03-12 11:28:19 +0700

    thêm alert và open graph preview

--- routes/index.js
+++ routes/index.js
@@ -8,17 +8,22 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/insert', async function (req, res, next) {
-  var user = new User({
-    name: req.body.name,
-    work: req.body.work,
-    phone: req.body.phone,
-    email: req.body.email
-  })
+  try {
+    var user = new User({
+      name: req.body.name,
+      work: req.body.work,
+      phone: req.body.phone,
+      email: req.body.email
+    })
 
-  // console.log(user)
+    // console.log(user)
 
-  await user.save()
-  res.redirect('/')
+    await user.save()
+    res.redirect('/?success=true');
+  } catch (error) {
+    console.log(error);
+    res.redirect('/?success=false');
+  }
 }, (err, req, res, next) => res.status(404).send({ error: err }))
 
 module.exports = router;
